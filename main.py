082e2c071f1eb22582a255e30e3c50473b5fb7f0
Ahmed Abusnaina <ceng.ahmed.kas@gmail.com>
2019-10-11 17:08:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,5 +37,8 @@ for available_text_manipulation in Available_Text_Manipulations:
             model = Train.Train(x_train,y_train,model=available_model, epochs = 20, batch_size = 256, LR = 0.001, n_layers = 1, layer_size = 128, filters = 64, dropout = True, MaxPooling = True, Embedding = Embed, vocab_size = vocab_size, embedding_dim = 300, loss = "binary_crossentropy")
 
             Train.Eval(x_test,y_test,model)
-            print(available_text_manipulation,available_class,available_model)
-            
+            model_json = model.to_json()
+            with open("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".json", "w") as json_file:
+                json_file.write(model_json)
+            model.save_weights("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".h5")
+            print("Saved model "+available_text_manipulation+"-"+available_class+"-"+available_model+" to disk")
