57f28c49c21b13241412bf09c80418af8a70d30d
Ahmed Abusnaina <ceng.ahmed.kas@gmail.com>
2019-10-14 15:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,30 +15,25 @@ x = data["comment_text"]
 x = x.values.tolist()
 x = Preprocess(x)
 
-Available_Classes = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-Available_Models = ['CNN','DNN','RNN','LSTM','GRU','Transformer']
-Available_Text_Manipulations = ['DOC2VEC','Embedding','USE']
+available_text_manipulation=sys.argv[1]
+available_class=sys.argv[2]
+available_model=sys.argv[3]
 
+Embed = False
+reshape = True
+if available_text_manipulation == "Embedding":
+    Embed = True
+    reshape = False
+Data_Representation, vocab_size = Manipulations_Selector(x,available_text_manipulation,vec_size=64,size=300)
+y = data[available_class]
+y = y.values.tolist()
+x_train, y_train, x_test, y_test = DataSeperator.Seperate(Data_Representation,y,0.8,reshape=reshape)
+if not(available_model == "Transformer" and available_text_manipulation != "Embedding"):
+    model = Train.Train(x_train,y_train,model=available_model, epochs = 20, batch_size = 256, LR = 0.001, n_layers = 1, layer_size = 128, filters = 64, dropout = True, MaxPooling = True, Embedding = Embed, vocab_size = vocab_size, embedding_dim = 300, loss = "binary_crossentropy")
 
-for available_text_manipulation in Available_Text_Manipulations:
-    Embed = False
-    reshape = True
-    if available_text_manipulation == "Embedding":
-        Embed = True
-        reshape = False
-    Data_Representation, vocab_size = Manipulations_Selector(x,available_text_manipulation,vec_size=64,size=300)
-    for available_class in Available_Classes:
-        y = data[available_class]
-        y = y.values.tolist()
-        x_train, y_train, x_test, y_test = DataSeperator.Seperate(Data_Representation,y,0.8,reshape=reshape)
-        for available_model in Available_Models:
-            if available_model == "Transformer" and available_text_manipulation != "Embedding":
-                continue
-            model = Train.Train(x_train,y_train,model=available_model, epochs = 20, batch_size = 256, LR = 0.001, n_layers = 1, layer_size = 128, filters = 64, dropout = True, MaxPooling = True, Embedding = Embed, vocab_size = vocab_size, embedding_dim = 300, loss = "binary_crossentropy")
-
-            Train.Eval(x_test,y_test,model)
-            model_json = model.to_json()
-            with open("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".json", "w") as json_file:
-                json_file.write(model_json)
-            model.save_weights("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".h5")
-            print("Saved model "+available_text_manipulation+"-"+available_class+"-"+available_model+" to disk")
+    Train.Eval(x_test,y_test,model)
+    model_json = model.to_json()
+    with open("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("./models/"+available_text_manipulation+"-"+available_class+"-"+available_model+".h5")
+    print("Saved model "+available_text_manipulation+"-"+available_class+"-"+available_model+" to disk")
